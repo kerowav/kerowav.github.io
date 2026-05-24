efd4609e5e16a38b1c1aeda04cd0dfb020526838
Kero <kero.mena1@gmail.com>
2026-05-23 21:34:15 -0700

    upated sudoku

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1669421864" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4234152268" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
