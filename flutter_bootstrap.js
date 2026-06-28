ca619e8b826baaf596c9705ef851462f437e2195
Kero <kero.mena1@gmail.com>
2026-06-28 11:42:23 -0700

    planner update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2688538361" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2308445062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
