cdc81487b0f7ff8cc03752fccd1bc93eee4e577c
Kero <kero.mena1@gmail.com>
2026-07-01 23:18:16 -0700

    update for name changing and profiles

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1124724206" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1500067411" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
